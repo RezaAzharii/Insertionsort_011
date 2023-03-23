86a094c67608777b33d8cc0dbe52ec88c139c8c8
RezaAzharii <rezaazhari867@gmail.com>
2023-03-23 10:33:03 +0700

    Membuat tampilan susunan data elemen array

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -18,4 +18,13 @@ void input() {						//Procedure Input
 			cout << "\n Array yang anda masukkan maksimal 20 elemen. \n"; // Menampilkan Pesan Jika  data lebih dari 20
 		}
 	}
+	cout << endl;									 // Membuat jarak perbaris program 
+	cout << "=====================" << endl;		 // Membuat tampilan susunan data elemen array
+	cout << "Masukan Element Array" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++)						 // Mengguanakan perualanga for untuk menyimpan data pada array
+	{
+
+	}
 }
