fa16c5d8d0011610ded20f3409a303c8aade45fb
RezaAzharii <rezaazhari867@gmail.com>
2023-03-23 10:27:23 +0700

    Membuat Inputan jumlah element Array

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -3,3 +3,19 @@ using namespace std;
 
 int arr[20];						// Membuat Array dengan panjang data 20
 int n;								// Membuat Variable inputan n
+
+void input() {						//Procedure Input
+	while (true)
+	{
+		cout << "Masukan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
+		cin >> n;									 // memanggil variable inputan n
+
+		if (n <= 20) {								 // membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\n Array yang anda masukkan maksimal 20 elemen. \n"; // Menampilkan Pesan Jika  data lebih dari 20
+		}
+	}
+}
